2eea6c99b35567341470f3b341ff8a05323f1faa
Manuel Ryan Espinosa <mico@nolimitsgallery.com>
2014-09-03 13:47:49 -0700

    Zillow - Javascript, CSS3 And SQL/MySQL

--- js_project/index.php
+++ js_project/index.php
@@ -6,9 +6,12 @@
 	<link rel="stylesheet" href="css/styles.css">
 </head>
 <body>
+	<header>
+		<h1>Javascript Demo<br><span>Expanding Textbox</span></h1>
+	</header>
 	<article>
 		<form action="">
-			<textarea rows="1" wrap="hard" >I am responive. Squish and Stretch me.</textarea>
+			<textarea rows="1" wrap="hard" placeholder="The Amazing Expanding Textbox."></textarea>
 		</form>
 	</article>
 	<script src="js/min/script-min.js"></script>
